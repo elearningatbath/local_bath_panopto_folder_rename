88bb34bc9d63acb09be1b66a62ec41a66ec69674
Hittesh Ahuja <ha386@bath.ac.uk>
2016-10-28 16:49:38 +0100

    Added curl options to __doRequest()

--- classes/SessionManagement.php
+++ classes/SessionManagement.php
@@ -60,6 +60,7 @@ class PanoptoSessionManagementSoapClient extends SoapClient{
 
     }
 
+
     /**
      *  Helper method for making a call to the Panopto API.
      *  $methodname is the case sensitive name of the API method to be called
@@ -158,7 +159,61 @@ class PanoptoSessionManagementSoapClient extends SoapClient{
      */
     public function __doRequest($request, $location, $action, $version, $oneway = null) {
         error_log(var_export($request,1));
-        return parent::__doRequest($request, $location, $this->currentaction, $version);
+
+
+        global $CFG;
+        //Attempt to intitialize cURL session to make SOAP calls.
+        $curl = curl_init($location);
+
+        //Check cURL was initialized
+        if ($curl !== false)
+        {
+            //Set standard cURL options
+            $options = array(
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_POST => true,
+                CURLOPT_POSTFIELDS => $request,
+                CURLOPT_NOSIGNAL => true,
+                CURLOPT_HTTPHEADER => array(sprintf('Content-Type: %s', $version == 2 ? 'application/soap+xml' : 'text/xml'), sprintf('SOAPAction: %s', $action)),
+                CURLOPT_SSL_VERIFYPEER => false, //All of our SOAP calls must be made via ssl
+                CURLOPT_TIMEOUT => 300 //Set call timeout in seconds
+            );
+            //Add curl options
+            $options[CURLOPT_PROXY] = $CFG->proxyhost;
+            $options[CURLOPT_PROXYPORT] = $CFG->proxyport;
+            $options[CURLOPT_PROXYTYPE] = (($CFG->proxytype === 'HTTP') ? CURLPROXY_HTTP : CURLPROXY_SOCKS5);
+            $option[CURLOPT_PROXYUSERPWD] = ((empty($CFG->proxypassword)) ? $CFG->proxyuser : "{$CFG->proxyuser}:{$CFG->proxypassword}");
+
+            //$options = array_merge(self::$curloptions, $options); // Add proxy options to curl request.
+            //Attempt to set the options for the cURL call
+            if (curl_setopt_array($curl, $options) !== false)
+            {
+                //Make call using cURL (including timeout settings)
+                $response = curl_exec($curl);
+                //If cURL throws an error, log it
+                if (curl_errno($curl) !== 0)
+                {
+                    error_log(curl_error($curl));
+                }
+            }
+            else
+            {
+                //A cURL option could not be set.
+                throw new Exception('Failed setting cURL options.');
+            }
+        }
+        else
+        {
+            //cURL was not initialized properly.
+            throw new Exception("Couldn't initialize cURL to make SOAP calls");
+        }
+
+        //Close cURL session.
+        curl_close($curl);
+
+        //Return the SOAP response
+        return $response;
+        //return parent::__doRequest($request, $location, $this->currentaction, $version);
     }
 
 }
